511b18e0ed75f1ba1681a8b34a9505a64a7fea57
Moritz <moritz-schneider@freenet.de>
2024-08-25 19:26:46 +0200

    Query Parameter Funktion und Button ohne Funktion

--- app.py
+++ app.py
@@ -4,43 +4,64 @@ import numpy as np
 import folium
 from streamlit_folium import st_folium
 
+# Initialisieren
+def init():
+    START_LOC = [49.01357217893837, 8.404385447502138]
+    START_ZOOM = 13
+
+    if 'marker_pos' not in st.session_state:
+        st.session_state.marker_pos = START_LOC
+    if 'zoom' not in st.session_state:
+        st.session_state.zoom = START_ZOOM
+
+# Karte erzeugen
+def generate_map():
+    """Karte mit Marker erzeugen. Lese Position aus session state"""
+    location = st.session_state.marker_pos
+    zoom = st.session_state.zoom
+    map_obj = folium.Map(location=location, zoom_start=zoom)
+    folium.Marker(location=location, tooltip="Marker Position").add_to(map_obj)
+    map_output = st_folium(map_obj, width=700, height=500)
+    return map_output
+
+# Query Parameter lesen
+def get_params():
+    query_params = st.query_params
+    if 'photo_id' in query_params:
+        photo_id = query_params['photo_id']
+    else:
+        photo_id = 'nA'
+    return photo_id
+
+def button_func():
+    st.session_state.payload = {
+        'guess_loc' : st.session_state.marker_pos
+    }
+
 # Hauptfunktion
 def main():
     st.title("KA-FotoFinder")
-    st.write("### Wähle einen Punkt auf der Karte aus und bestätige ihn")
+    col1, col2 = st.columns(spec=[0.7, 0.3])
+    with col1:
+        st.write("### Ort auswählen und bestägigen")
+    with col2:
+        st.button("Abschicken", type="primary", on_click=button_func(), use_container_width=True)
+    
+    # Verarbeite Parameter aus QR-Code
+    photo_id = get_params()
 
-    # Start Koordinaten
-    start_loc = [51.1657, 10.4515]
-    start_zoom = 13
-
-    # Session State
-    if 'marker_pos' not in st.session_state:
-        st.session_state.marker_pos = start_loc
-    if 'zoom' not in st.session_state:
-        st.session_state.zoom = start_zoom
-    st.write(f"Marker Position: Latitude: {st.session_state.marker_pos[0]}, Longitude: {st.session_state.marker_pos[1]}")
-
-    # Karte definieren
-    map_obj = folium.Map(location=st.session_state.marker_pos, zoom_start=st.session_state.zoom)
-
-    ## Marker definieren
-    folium.Marker(st.session_state.marker_pos, tooltip="Marker Position").add_to(map_obj)
-
-    # Karte anzeigen
-    map_output = st_folium(map_obj, width=700, height=500)
+    # Karte Initial erstellen
+    map_output = generate_map()
 
     # Verarbeite die Klicks auf der Karte
     if map_output and map_output.get('last_clicked'):
-        lat = map_output['last_clicked']['lat']
-        lon = map_output['last_clicked']['lng']
-        zoom = map_output['zoom']
-        st.session_state.marker_pos = [lat, lon]  # Update marker position
-        st.session_state.zoom = zoom
-
-        # Karte neu erstellen, um die aktualisierte Marker-Position und Zoom-Stufe zu reflektieren
-        map_obj = folium.Map(location=st.session_state.marker_pos, zoom_start=st.session_state.zoom)
-        folium.Marker(st.session_state.marker_pos, tooltip="Marker Position").add_to(map_obj)
-        st_folium(map_obj, width=700, height=500)
+        st.session_state.marker_pos = [map_output['last_clicked']['lat'], map_output['last_clicked']['lng']]
+        st.session_state.zoom = map_output['zoom']
+        map_output = generate_map()
+    
+    # Output für Entwicklung
+    st.write(f"Marker Position: Latitude: {st.session_state.marker_pos[0]}, Longitude: {st.session_state.marker_pos[1]}, Foto: {photo_id}, Payload = {st.session_state.payload}")
 
 if __name__ == "__main__":
+    init()
     main()

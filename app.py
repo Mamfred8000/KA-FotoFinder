cb7ce41fe2e1f9870e8e15f3d1fe5ecb1445fcbb
Moritz <moritz-schneider@freenet.de>
2024-08-26 21:47:36 +0200

    Bug fix: doppelte Anzeige von Karte; Initialisierung verbessert

--- app.py
+++ app.py
@@ -3,20 +3,21 @@ import pandas as pd
 import numpy as np
 import folium
 from streamlit_folium import st_folium
+from streamlit import runtime
+from streamlit.runtime.scriptrunner import get_script_run_ctx
 
 # Initialisieren
 def init():
-    START_LOC = [49.01357217893837, 8.404385447502138]
-    START_ZOOM = 13
-
-    if 'marker_pos' not in st.session_state:
-        st.session_state.marker_pos = START_LOC
-    if 'zoom' not in st.session_state:
-        st.session_state.zoom = START_ZOOM
+    st.session_state.init_flag = True
+    
+    st.session_state.marker_pos = [49.01357217893837, 8.404385447502138]
+    st.session_state.zoom = 13
+    st.session_state.counter = 1
+    st.session_state.guess_position = [0, 0]
 
 # Karte erzeugen
 def generate_map():
-    """Karte mit Marker erzeugen. Lese Position aus session state"""
+    """Karte mit Marker erzeugen. Lese Position aus session state"""    
     location = st.session_state.marker_pos
     zoom = st.session_state.zoom
     map_obj = folium.Map(location=location, zoom_start=zoom)
@@ -34,9 +35,21 @@ def get_params():
     return photo_id
 
 def button_func():
-    st.session_state.payload = {
-        'guess_loc' : st.session_state.marker_pos
-    }
+    st.session_state.counter += 1
+    st.session_state.guess_position = st.session_state.marker_pos
+
+def get_remote_ip() -> str:
+    """Get remote ip."""
+    try:
+        ctx = get_script_run_ctx()
+        if ctx is None:
+            return None
+        session_info = runtime.get_instance().get_client(ctx.session_id)
+        if session_info is None:
+            return None
+    except Exception as e:
+        return None
+    return session_info.request.remote_ip
 
 # Hauptfunktion
 def main():
@@ -45,7 +58,7 @@ def main():
     with col1:
         st.write("### Ort auswählen und bestägigen")
     with col2:
-        st.button("Abschicken", type="primary", on_click=button_func(), use_container_width=True)
+        st.button("Abschicken", type="primary", on_click=button_func, use_container_width=True)
     
     # Verarbeite Parameter aus QR-Code
     photo_id = get_params()
@@ -54,14 +67,16 @@ def main():
     map_output = generate_map()
 
     # Verarbeite die Klicks auf der Karte
-    if map_output and map_output.get('last_clicked'):
+    if  map_output and map_output.get('last_clicked'):
         st.session_state.marker_pos = [map_output['last_clicked']['lat'], map_output['last_clicked']['lng']]
         st.session_state.zoom = map_output['zoom']
-        map_output = generate_map()
+        st.rerun()
     
     # Output für Entwicklung
-    st.write(f"Marker Position: Latitude: {st.session_state.marker_pos[0]}, Longitude: {st.session_state.marker_pos[1]}, Foto: {photo_id}, Payload = {st.session_state.payload}")
+    st.write(f"Marker Position: Latitude: {st.session_state.marker_pos[0]}, Longitude: {st.session_state.marker_pos[1]}, Foto: {photo_id}")
+    st.write(f"Button Click Counter: {st.session_state.counter}, Guess Position: {st.session_state.guess_position}")
+    st.write(f"The remote ip is {get_remote_ip()}")
 
 if __name__ == "__main__":
-    init()
+    if 'init_flag' not in st.session_state: init()
     main()

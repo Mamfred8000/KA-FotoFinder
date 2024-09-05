d99e4561328107b520b6595d3f76a8b6ac39d323
Moritz <moritz-schneider@freenet.de>
2024-09-05 21:33:36 +0200

    Home BIldschirm aufgebaut

--- app.py
+++ app.py
@@ -1,21 +1,7 @@
 import time
 import streamlit as st
-#import pandas as pd
-#import numpy as np
-import folium
-from streamlit_folium import st_folium
 from streamlit import runtime
-from streamlit.runtime.scriptrunner import get_script_run_ctx
 from streamlit_javascript import st_javascript
-
-# Initialisieren
-def init():
-    st.session_state.init_flag = True
-    
-    st.session_state.marker_pos = [49.01357217893837, 8.404385447502138]
-    st.session_state.zoom = 13
-    st.session_state.counter = 1
-    st.session_state.guess_position = [0, 0]
 
 def init_deviceMode():
     user_agent = st_javascript("window.navigator.userAgent")
@@ -32,19 +18,8 @@ def init_deviceMode():
         else:
             time.sleep(0.01)
 
-# Karte erzeugen
-def generate_map():
-    """Karte mit Marker erzeugen. Lese Position aus session state"""
-    width, height = (300, 400) if st.session_state.device_mode == "mobile" else (700, 500)
-    location = st.session_state.marker_pos
-    zoom = st.session_state.zoom
-    map_obj = folium.Map(location=location, zoom_start=zoom)
-    folium.Marker(location=location, tooltip="Marker Position").add_to(map_obj)
-    map_output = st_folium(map_obj, width=width, height=height)
-    return map_output
-
 # Query Parameter lesen
-def get_params():
+def get_photo_param():
     query_params = st.query_params
     if 'photo_id' in query_params:
         photo_id = query_params['photo_id']
@@ -52,42 +27,33 @@ def get_params():
         photo_id = 'nA'
     return photo_id
 
-def button_func():
-    st.session_state.counter += 1
-    st.session_state.guess_position = st.session_state.marker_pos
+def init():
+    st.session_state.init_flag = True
+    st.session_state.user_name = ""
+    st.session_state.marker_pos = [49.01357217893837, 8.404385447502138]
+    st.session_state.zoom = 13
+    st.session_state.counter = 1
+    st.session_state.guess_position = [0, 0]
+    st.session_state.photo_id = get_photo_param()
 
 # Hauptfunktion
 def main():
-    showSidebarNavigation = false
     st.title("KA-FotoFinder")
-    col1, col2 = st.columns(spec=[0.7, 0.3])
-    with col1:
-        st.write("### Ort auswählen und bestägigen")
-    with col2:
-        st.button("Abschicken", type="primary", on_click=button_func, use_container_width=True)
-    
-    # Verarbeite Parameter aus QR-Code
-    photo_id = get_params()
 
-    # Karte Initial erstellen
-    map_output = generate_map()
-
-    # Verarbeite die Klicks auf der Karte
-    if  map_output and map_output.get('last_clicked'):
-        st.session_state.marker_pos = [map_output['last_clicked']['lat'], map_output['last_clicked']['lng']]
-        st.session_state.zoom = map_output['zoom']
-        st.rerun()
-    
-    # Output für Entwicklung
-    st.write(f"Marker Position: Latitude: {st.session_state.marker_pos[0]}, Longitude: {st.session_state.marker_pos[1]}, Foto: {photo_id}")
-    st.write(f"Button Click Counter: {st.session_state.counter}, Guess Position: {st.session_state.guess_position}")
-    st.write(f"Device mode: {st.session_state.device_mode}")
-
-    if st.button("Quiz"):
-        st.switch_page("pages/1_quiz.py")
+    if st.session_state.user_name:
+        st.write(f"## Hallo {st.session_state.user_name}!")
+        if st.button("Starten", type="primary"): st.switch_page("pages/1_quiz.py")
+    else:
+        st.write("## Neu hier...?")
+        name = st.text_input("Wie heißt du?", placeholder="Name")
+        if name:
+            st.session_state.user_name = name
+            st.rerun()
 
 if __name__ == "__main__":
-    if 'init_flag' not in st.session_state:
+    if 'init_flag' in st.session_state:
+        main()
+    else:
         init_deviceMode()
         init()
-    main()
+        st.rerun()
--- pages/1_quiz.py
+++ pages/1_quiz.py
@@ -1,6 +1,48 @@
 import streamlit as st
+import folium
+from streamlit_folium import st_folium
 
-st.title("quiz")
+# Karte erzeugen
+def generate_map():
+    """Karte mit Marker erzeugen. Lese Position aus session state"""
+    width, height = (300, 400) if st.session_state.device_mode == "mobile" else (700, 500)
+    location = st.session_state.marker_pos
+    zoom = st.session_state.zoom
+    map_obj = folium.Map(location=location, zoom_start=zoom)
+    folium.Marker(location=location, tooltip="Marker Position").add_to(map_obj)
+    map_output = st_folium(map_obj, width=width, height=height)
+    return map_output
 
-if st.button("Home"):
-    st.switch_page("app.py")
+def button_func():
+    st.session_state.counter += 1
+    st.session_state.guess_position = st.session_state.marker_pos
+
+def main():
+    st.title("quiz")
+
+    col1, col2 = st.columns(spec=[0.7, 0.3])
+    with col1:
+        st.write("### Ort auswählen und bestätigen")
+    with col2:
+        st.button("Abschicken", type="primary", on_click=button_func, use_container_width=True)
+    
+    # Karte Initial erstellen
+    map_output = generate_map()
+
+    # Verarbeite die Klicks auf der Karte
+    if  map_output and map_output.get('last_clicked'):
+        st.session_state.marker_pos = [map_output['last_clicked']['lat'], map_output['last_clicked']['lng']]
+        st.session_state.zoom = map_output['zoom']
+        st.rerun()
+    
+    # Output für Entwicklung
+    st.write(st.session_state)
+
+    if st.button("Go Back Home"):
+        st.switch_page("app.py")
+
+##main
+if 'init_flag' in st.session_state:
+    main()
+else:
+    st.write("Initialization failed. Read QR-Code to resume.")

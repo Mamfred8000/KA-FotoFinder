98d8196c94e32614f05bdc0f3d3fea3d8a3771a0
Moritz <moritz-schneider@freenet.de>
2024-09-29 18:18:07 +0200

    An Datenbank gearbeitet

--- pages/3_database.py
+++ pages/3_database.py
@@ -1,17 +1,7 @@
 import streamlit as st
 from datetime import date
 from sqlalchemy import text
-
-st.title("Datenbank")
-
-# Initialize connection.
-conn = st.connection("postgresql", type="sql")
-
-# User input fields
-username = st.text_input("Username")
-timestamp = st.date_input("Timestamp", date.today())
-distance = st.number_input("Distance (in meters)", min_value=0.0, format="%.2f")
-photo_id = st.text_input("Photo ID")
+import time
 
 # Function to insert data into the database
 def insert_data(username, timestamp, distance, photo_id):
@@ -20,26 +10,38 @@ def insert_data(username, timestamp, distance, photo_id):
         'VALUES (:username, :timestamp, :distance, :photo_id)'
     )
     
-    # Execute the insert query
-    with conn.session as s:
-        s.execute(insert_query, {
-            'username': username,
-            'timestamp': timestamp,
-            'distance': distance,
-            'photo_id': photo_id
-        })
-        s.commit()
+    # Execute the insert query with a progress spinner
+    with st.spinner('Warum auch immer ist die Datenbank Latenz relativ hoch...'):
+        with conn.session as s:
+            s.execute(insert_query, {
+                'username': username,
+                'timestamp': timestamp,
+                'distance': distance,
+                'photo_id': photo_id
+            })
+            s.commit()
+            time.sleep(4)  #Latency of database is 1-4 seconds
     
     st.success("Data inserted successfully!")
-    st.rerun()
+
+### MAIN
+conn = st.connection("postgresql", type="sql")
+
+st.title("Datenbank")
+
+username = st.text_input("Username")
+timestamp = st.date_input("Timestamp", date.today())
+distance = st.number_input("Distance (in meters)", min_value=0.0, format="%.2f")
+photo_id = st.text_input("Photo ID")
 
 # Button to submit the data
 if st.button("Submit"):
     if username and photo_id:  # Basic validation to ensure fields are filled
         insert_data(username, timestamp, distance, photo_id)
+        st.rerun()
     else:
         st.warning("Please fill in all fields.")
 
-# Perform query.
-df = conn.query('SELECT * FROM "KAFotoFinder-Scoreboard";', ttl="10m")
+# Query the database for the updated data, caching for ttl seconds
+df = conn.query('SELECT * FROM "KAFotoFinder-Scoreboard";', ttl=5)
 st.write(df)
